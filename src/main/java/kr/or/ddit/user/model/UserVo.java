4d5483fe59597e8c2b9e3eb74a812450a2336d03
hskim <hs957474@gmail.com>
2021-01-29 12:34:47 +0900

    type conversion  . conversionService  . formattingConversionService

--- src/main/java/kr/or/ddit/user/model/UserVo.java
+++ src/main/java/kr/or/ddit/user/model/UserVo.java
@@ -1,8 +1,22 @@
 package kr.or.ddit.user.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
 public class UserVo {
 	private String userid;
 	private String usernm;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date reg_dt;
+	
+	@DateTimeFormat(pattern = "MM-dd")
+	private Date hire_dt;
+	
+	@NumberFormat(pattern = "#,###")
+	private int price;
 	
 	public UserVo() {}
 
@@ -10,7 +24,25 @@ public class UserVo {
 		setUserid(userid);
 		setUsernm(usernm);
 	}
-
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getHire_dt() {
+		return hire_dt;
+	}
+	public void setHire_dt(Date hire_dt) {
+		this.hire_dt = hire_dt;
+	}
+	public Date getReg_dt() {
+		return reg_dt;
+	}
+	public void setReg_dt(Date reg_dt) {
+		this.reg_dt = reg_dt;
+	}
 	public String getUserid() {
 		return userid;
 	}
@@ -23,9 +55,12 @@ public class UserVo {
 	public void setUsernm(String usernm) {
 		this.usernm = usernm;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "UserVo [userid=" + userid + ", usernm=" + usernm + "]";
+		return "UserVo [userid=" + userid + ", usernm=" + usernm + ", reg_dt=" + reg_dt + ", hire_dt=" + hire_dt
+				+ ", price=" + price + "]";
 	}
+
+	
 }
